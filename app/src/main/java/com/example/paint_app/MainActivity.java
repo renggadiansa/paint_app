bb8733550d7c906cb739978ac1aff11e434147b6
renggadiansa <renggadiansa@gmail.com>
2022-12-03 17:30:59 +0700

    first commit

---
+++ app/src/main/java/com/example/paint_app/MainActivity.java
@@ -0,0 +1,166 @@
+package com.example.paint_app;
+
+import android.Manifest;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.github.gcacace.signaturepad.views.SignaturePad;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.DexterBuilder;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    int defaultColor;
+    SignaturePad signatureView;
+    ImageButton imgEraser, imgColor, imgSave;
+    SeekBar seekBar;
+    TextView txtPenSize;
+
+    private static String fileName;
+    File path = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/PaintApp/");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        signatureView = findViewById(R.id.signature_pad);
+        seekBar = findViewById(R.id.pen_size);
+        txtPenSize = findViewById(R.id.pen_size_text);
+        imgColor = findViewById(R.id.btn_color);
+        imgEraser = findViewById(R.id.btn_eraser);
+        imgSave = findViewById(R.id.btn_save);
+
+        askPermission();
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault());
+        String date = format.format(new Date());
+        fileName = path + "/" + date + ".png";
+
+        if (!path.exists())
+        {
+            path.mkdirs();
+        }
+
+        defaultColor = ContextCompat.getColor(MainActivity.this, R.color.black);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                txtPenSize.setText(i + "dp");
+                signatureView.setMaxWidth(i);
+                signatureView.setMinWidth(i);
+                seekBar.setMax(50);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        imgColor.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                openColorPicker();
+            }
+        });
+        imgEraser.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                signatureView.clear();
+            }
+        });
+        imgSave.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (!signatureView.isEmpty()) {
+                    try {
+                        saveImage();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this, "Gambar gagal disimpan", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void saveImage() throws IOException {
+        File file = new File(fileName);
+        Bitmap bitmap = signatureView.getSignatureBitmap();
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos);
+        byte[] bitmapData = bos.toByteArray();
+
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(bitmapData);
+        fos.flush();
+        fos.close();
+        Toast.makeText(this, "Gambar disimpan", Toast.LENGTH_SHORT).show();
+    }
+
+    private void openColorPicker() {
+        AmbilWarnaDialog ambilWarnaDialog = new AmbilWarnaDialog(this, defaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+              //  Toast.makeText(MainActivity.this, "Color Picker Closed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                defaultColor = color;
+                signatureView.setPenColor(color);
+            }
+        });
+        ambilWarnaDialog.show();
+    }
+
+    private void askPermission() {
+        Dexter.withContext(this)
+                .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        //Toast.makeText(MainActivity.this, "sukses", Toast.LENGTH_SHORT).show();
+
+                    }
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+}
